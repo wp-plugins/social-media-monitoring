da61e02585dd341bd983a4d3583e49b69ed2bbef
rajatgarg <rajatgarg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-31 09:40:22 +0000

    Version 1.2 - Major upgrade - customize look & feel, also show widget below every post
    git-svn-id: https://plugins.svn.wordpress.org/social-media-monitoring/trunk@646326 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- monitoring.php
+++ monitoring.php
@@ -9,6 +9,7 @@ Author URI: https://www.socialappshq.com/
 */
 
 require_once 'php/admin.php';
+require_once 'php/enhance_post.php';
 
 if (!class_exists("MonitoringWidget")) {
     class MonitoringWidget extends WP_Widget
@@ -56,11 +57,25 @@ if (!class_exists("MonitoringWidget")) {
 
         // WIDGET CODE GOES HERE
         $monitoring_variables = get_option('MonitoringAdminAdminOptions');
+
+        $num_posts = $monitoring_variables['shq_c_po'];
+        if (intval($num_posts)==0) {
+            $num_posts = 3;
+        }
+        $iframe_height = intval($num_posts)*80+180;
+
+        // configuration section
+        $config_sidebar = "";
+        $config_sidebar = "&c_b=".$monitoring_variables['shq_c_b']."&c_i_b=".$monitoring_variables['shq_c_i_b']."&c_f=".$monitoring_variables['shq_c_f']."&c_h_c=".$monitoring_variables['shq_c_h_c']."&c_bg=".$monitoring_variables['shq_c_bg']."&c_f_c=".$monitoring_variables['shq_c_f_c']."&c_po=".$num_posts;
+
+
 	
-		echo "<div style='text-align:center;width: 282px;'><iframe id='shq-iframe-1355914507' style='height: 402px; width: 272px;border:none;' src='https://widget.socialappshq.com/widget/?q=".$monitoring_variables['keyword']."&size=10&men=true&sent=true&lm=true&inf=false&c=NDA1Njc0%0A&em=".$monitoring_variables['email']."'></iframe><script>var _0x8e15=['src','shq-iframe-1356330352','getElementById','&s=','domain'];document[_0x8e15[2]](_0x8e15[1])[_0x8e15[0]]=document[_0x8e15[2]](_0x8e15[1])[_0x8e15[0]]+_0x8e15[3]+document[_0x8e15[4]];</script>";
+	echo "<div style='text-align:center;width: 282px;'><iframe id='shq-iframe-1355914507' style='height:".$iframe_height."px; width: 272px;border:none;' src='https://widget.socialappshq.com/widget/?q=".$monitoring_variables['keyword']."&size=10&men=true&sent=true&lm=true&inf=false&c=NDA1Njc0%0A&em=".$monitoring_variables['email'].$config_sidebar."'></iframe><script>var _0x8e15=['src','shq-iframe-1355914507','getElementById','&s=','domain'];document[_0x8e15[2]](_0x8e15[1])[_0x8e15[0]]=document[_0x8e15[2]](_0x8e15[1])[_0x8e15[0]]+_0x8e15[3]+document[_0x8e15[4]];</script>";
 
         if(($monitoring_variables['show_socialapp_link']==1)){
-            echo "<br/><span style='font-size:12px;font-family:arial;'><a href='https://www.socialappshq.com/social-media-monitoring' style='cursor:pointer;text-decoration:none;color:#3F6EBA;' target='_blank'>Online Monitoring</a> by <a href='https://www.socialappshq.com' target='_blank' style='cursor:pointer;text-decoration:none;color:#3F6EBA;'>SocialAppsHQ</a></span></div>";
+            echo "<br/><span style='font-size:10px;font-family:arial;'><a href='https://www.socialappshq.com/social-media-monitoring' style='cursor:pointer;text-decoration:none;color:#3F6EBA;' target='_blank'>Online Monitoring</a> by <a href='https://www.socialappshq.com' target='_blank' style='cursor:pointer;text-decoration:none;color:#3F6EBA;'>SocialAppsHQ</a></span></div>";
+        } else {
+            echo "</div>";
         }
 
         echo $after_widget;
@@ -69,7 +84,56 @@ if (!class_exists("MonitoringWidget")) {
     }
 }
 
+function show_widget_at_bottom($content)
+{
+        // WIDGET CODE GOES HERE
+        $monitoring_variables = get_option('MonitoringAdminAdminOptions');
+        global $wp_query;
+        $postid = $wp_query->post->ID;
+
+        $search_keyword =  get_post_meta($postid, 'monitoring_text', true);
+        // for the global search keyword
+        if (empty($search_keyword)) {
+            $search_keyword = $monitoring_variables['keyword'];
+        }
+        // see if we can get tracking for URL
+        if (empty($search_keyword)) {
+            $search_keyword = get_permalink( $postid );
+        }
+
+        // see if it disabled on this post
+        $is_disabled = get_post_meta($postid, 'monitoring_checkbox', true);
+        if ($is_disabled =='on') {
+            return $content;
+        }
+        $num_posts = get_post_meta($postid, 'monitoring_posts', true);
+        if (intval($num_posts)==0) {
+            $num_posts = 10;
+        }
+        $iframe_height = intval($num_posts)*80+195;
+
+        // configuration section
+        $config = "";
+        $config = "&c_b=".$monitoring_variables['w_shq_c_b']."&c_i_b=".$monitoring_variables['w_shq_c_i_b']."&c_f=".$monitoring_variables['w_shq_c_f']."&c_h_c=".$monitoring_variables['w_shq_c_h_c']."&c_bg=".$monitoring_variables['w_shq_c_bg']."&c_f_c=".$monitoring_variables['w_shq_c_f_c']."&c_po=".$num_posts;
+
+        //$tags = the_tags();
+	// this is where we will display the bio
+	if ( is_single() || is_page() )
+	{
+		$widget_bottom = "<div style='width: 612px;margin-top:20pxmargin-bottom:20px'><iframe id='shq-iframe-1356698176' style='height:".$iframe_height."px; width: 602px;border:none;' src='https://widget.socialappshq.com/widget/?q=".$search_keyword."&size=11&men=true&sent=true&lm=true&inf=false&c=NDA1Njc0%0A&em=".$monitoring_variables['email'].$config."'></iframe><script>var _0x8e15=['src','shq-iframe-1356698176','getElementById','&s=','domain'];document[_0x8e15[2]](_0x8e15[1])[_0x8e15[0]]=document[_0x8e15[2]](_0x8e15[1])[_0x8e15[0]]+_0x8e15[3]+document[_0x8e15[4]];</script>";
+                if(($monitoring_variables['show_socialapp_link']==1)){
+                    $widget_bottom = $widget_bottom . "<br/><span style='font-size:10px;font-family:arial;float:right;margin-right:10px;'><a href='https://www.socialappshq.com/social-media-monitoring' style='cursor:pointer;text-decoration:none;color:#3F6EBA;' target='_blank'>Online Monitoring</a> by <a href='https://www.socialappshq.com' target='_blank' style='cursor:pointer;text-decoration:none;color:#3F6EBA;'>SocialAppsHQ</a></span></div>";
+                } else {
+                    $widget_bottom = $widget_bottom . "</div>";
+                }
+		return $content . $widget_bottom;
+	} else {
+		return $content;
+	}
+}
+
 add_action( 'widgets_init', create_function('', 'return register_widget("MonitoringWidget");') );
 add_action( 'admin_menu', 'my_monitoring_menu' );
+add_action( 'the_content', 'show_widget_at_bottom');
 
 ?>
--- php/admin.php
+++ php/admin.php
@@ -11,7 +11,7 @@
 		}
 		//Returns an array of admin options
 		function getAdminOptions() {
-			$devloungeAdminOptions = array('keyword' => '', credits => '');
+			$devloungeAdminOptions = array('keyword' => '', credits => '', 'shq_c_b' => 'CCCCCC', 'shq_c_i_b' => 'CCCCCC', 'shq_c_f' => 'arial,sans-serif', 'shq_c_h_c' => '3F6EBA', 'shq_c_f_c' => '000000', 'shq_c_bg' => 'ffffff', 'w_shq_c_b' => 'CCCCCC', 'w_shq_c_i_b' => 'CCCCCC', 'w_shq_c_f' => 'arial, sans_serif', 'w_shq_c_h_c' => '3F6EBA', 'w_shq_c_f_c' => '000000', 'w_shq_c_bg' => 'ffffff', 'shq_po' => 3);
 			$devOptions = get_option($this->adminOptionsName);
 			if (!empty($devOptions)) {
 				foreach ($devOptions as $key => $option)
@@ -42,6 +42,49 @@
                         if (isset($_POST['devloungeEmail'])) {
                                 $devOptions['email'] = apply_filters('keyword_save_pre', $_POST['devloungeEmail']);
                         }
+
+                        if (isset($_POST['devloungeShowBorder'])) {
+                                $devOptions['shq_c_b'] = apply_filters('keyword_save_pre', $_POST['devloungeShowBorder']);
+                        }
+                        if (isset($_POST['devloungeInnerBorder'])) {
+                                $devOptions['shq_c_i_b'] = apply_filters('keyword_save_pre', $_POST['devloungeInnerBorder']);
+                        }
+                        if (isset($_POST['devloungeFontType'])) {
+                                $devOptions['shq_c_f'] = apply_filters('keyword_save_pre', $_POST['devloungeFontType']);
+                        }
+                        if (isset($_POST['devloungeHeaderColor'])) {
+                                $devOptions['shq_c_h_c'] = apply_filters('keyword_save_pre', $_POST['devloungeHeaderColor']);
+                        }
+                        if (isset($_POST['devloungeBackgroundColor'])) {
+                                $devOptions['shq_c_bg'] = apply_filters('keyword_save_pre', $_POST['devloungeBackgroundColor']);
+                        }
+                        if (isset($_POST['devloungeFontColor'])) {
+                                $devOptions['shq_c_f_c'] = apply_filters('keyword_save_pre', $_POST['devloungeFontColor']);
+                        }
+                        if (isset($_POST['devloungePosts'])) {
+                                $devOptions['shq_c_po'] = apply_filters('keyword_save_pre', $_POST['devloungePosts']);
+                        }
+
+                        if (isset($_POST['devloungeShowBorder'])) {
+                                $devOptions['w_shq_c_b'] = apply_filters('keyword_save_pre', $_POST['devloungeWShowBorder']);
+                        }
+                        if (isset($_POST['devloungeInnerBorder'])) {
+                                $devOptions['w_shq_c_i_b'] = apply_filters('keyword_save_pre', $_POST['devloungeWInnerBorder']);
+                        }
+                        if (isset($_POST['devloungeFontType'])) {
+                                $devOptions['w_shq_c_f'] = apply_filters('keyword_save_pre', $_POST['devloungeWFontType']);
+                        }
+                        if (isset($_POST['devloungeHeaderColor'])) {
+                                $devOptions['w_shq_c_h_c'] = apply_filters('keyword_save_pre', $_POST['devloungeWHeaderColor']);
+                        }
+                        if (isset($_POST['devloungeBackgroundColor'])) {
+                                $devOptions['w_shq_c_bg'] = apply_filters('keyword_save_pre', $_POST['devloungeWBackgroundColor']);
+                        }
+                        if (isset($_POST['devloungeFontColor'])) {
+                                $devOptions['w_shq_c_f_c'] = apply_filters('keyword_save_pre', $_POST['devloungeWFontColor']);
+                        }
+
+
                         if ((isset($_POST['devloungeCredits'])) && (1==$_POST['devloungeCredits'])) {
                             $devOptions['show_socialapp_link'] = 1;
                         } else {
@@ -55,33 +98,163 @@
                         <?php
                         } ?>
 
+                        <style>
+
+                            .wrap h3{float: left;width:400px;margin:10px 10px 0 0;font-weight:normal}
+                            .wrap p{float: left; max-width:300px;margin:0}
+                            .wrap small{float: left; width:100%;margin:0}
+                            .wrap .input_color{width:200px}
+                            .wrap .input_kwd{width:200px; margin-left:10px;}
+                            .wrap .row{float:left;width:100%; margin:10px 0;}
+                            .wrap .title{font-weight:bold;margin:20px 0}
+                            .wrap .input_font{margin-left:10px; width:200px}
+                            .wrap .che{margin:0px; -webkit-border-radius:0px  !important;-moz-border-radius:0px  !important;border-radius:0px !important; }
+                            .wrap p input{background-color: #ffffff;
+                                          border: 1px solid #cccccc;
+                                          -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+                                          -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+                                          box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+                                          -webkit-transition: border linear .2s, box-shadow linear .2s;
+                                          -moz-transition: border linear .2s, box-shadow linear .2s;
+                                          -o-transition: border linear .2s, box-shadow linear .2s;
+                                          transition: border linear .2s, box-shadow linear .2s; margin-top:5px; -webkit-border-radius:10px;
+                                        -moz-border-radius:10px;border-radius:10px;
+                                        }
+                            .wrap p input:hover{border-color: rgba(82, 168, 236, 0.8);
+                              outline: 0;
+                              outline: thin dotted \9;
+                              /* IE6-9 */
+
+                              -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(82,168,236,.6);
+                              -moz-box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(82,168,236,.6);
+                              box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(82,168,236,.6);
+                            }
+
+                            .btn-primary {
+                              color: #ffffff !important;
+                              text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25) !important;
+                              background-color: #006dcc !important;
+                              background-image: -moz-linear-gradient(top, #0088cc, #0044cc)!important;
+                              background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#0088cc), to(#0044cc))!important;
+                              background-image: -webkit-linear-gradient(top, #0088cc, #0044cc)!important;
+                              background-image: -o-linear-gradient(top, #0088cc, #0044cc)!important;
+                              background-image: linear-gradient(to bottom, #0088cc, #0044cc)!important;
+                              background-repeat: repeat-x;
+                              filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff0088cc', endColorstr='#ff0044cc', GradientType=0);
+                              border-color: #0044cc #0044cc #002a80 !important;
+                              border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25)!important;
+                              *background-color: #0044cc !important;
+                              /* Darken IE7 buttons by default so they stand out more given they won't have borders */
+
+                              filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);-webkit-border-radius:10px;-moz-border-radius:10px;border-radius:10px;
+                            }
+                            .btn-primary:hover{
+                             color: #ffffff !important;
+                              background-color: #0044cc !important; }
+
+
+
+                        </style>
                         <div class="wrap">
                             <div style="float:left;width:60%">
                                 <form method="post" action="/wp-admin/admin.php?page=monitoring-menu-id">
 
                                 <h2>Admin Panel for Monitoring Plugin</h2>
-                                <h3 style="float: left; width:100%;">Enter Keywords that you want to track across the web</h3>
-                                <p style="float: left; width:100%;clear: both;margin:0">You can enter comma separated keywords below</p>
-                                <p style="float: left; clear: both;margin:0"><input name="devloungeContent" style="width: 500px; float:left" value="<?php echo $devOptions['keyword'] ?>"></p>
-                                <br/>
-                                <h3 style="float: left; width:100%;">Enter Email Address</h3>
-                                <p style="float: left; width:100%;clear: both;margin:0">You can use this email to access raw data collected for keywords on SocialAppsHQ</p>
-                                <p style="float: left; clear: both;margin:0"><input name="devloungeEmail" style="width: 500px; float:left" value="<?php echo get_option('admin_email') ?>"></p>
-                                <br/>
-                                <p style="float: left; width:100%;clear: both;margin-top:10px">                                    
-                                    <input type="checkbox" name="devloungeCredits" value="1" <?php if(isset($_POST['devloungeCredits'])){  ?>checked="checked"<? }php ?>  /> Show Credits                                    
-                                </p>
                                 
+                                <div class="row">
+                                <h3>Enter Keywords that you want to track across the web</h3>
+                                <p><input name="devloungeContent" class="input_kwd" value="<?php echo $devOptions['keyword'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Enter Email Address</h3>
+                                <p><input name="devloungeEmail" class="input_kwd" value="<?php echo get_option('admin_email') ?>"></p>
+                                <small>You can use this email to access raw data collected for keywords on SocialAppsHQ</small>
+                                </div>
+                                <div class="row">
+
+                                <h3 class="title">Configure Look and Feel of Sidebar Widget</h3>
+                                </div>
+                                <div class="row">
+
+                                <h3>Color of Border</h3>
+                                <p>#<input name="devloungeShowBorder" class="input_color" value="<?php echo $devOptions['shq_c_b'] ?>"></p>
+                                <small>Enter Color hexcode without #. You can enter 0 for hiding border.</small>
+                                </div>
+                                <div class="row">
+                                
+                                <h3>Color of Inner Lines</h3>
+                                <p>#<input name="devloungeInnerBorder" class="input_color" value="<?php echo $devOptions['shq_c_i_b'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Font Type</h3>
+                                <p><input name="devloungeFontType" class="input_font" value="<?php echo $devOptions['shq_c_f'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Header Font Color</h3>
+                                <p>#<input name="devloungeHeaderColor" class="input_color" value="<?php echo $devOptions['shq_c_h_c'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Background Color</h3>
+                                <p>#<input name="devloungeBackgroundColor" class="input_color" value="<?php echo $devOptions['shq_c_bg'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Font Color</h3>
+                                <p>#<input name="devloungeFontColor" class="input_color" value="<?php echo $devOptions['shq_c_f_c'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Number of Posts</h3>
+                                <p>#<input name="devloungePosts" class="input_color" value="<?php echo $devOptions['shq_c_po'] ?>"></p>
+                                <small>Number of posts to show in sidebar. Allowed range is between 2 to 10.</small>
+                                </div>
+
+                                <div class="row">
+                                <h3 class="title">Configure Look and Feel of Widget below Posts</h3>
+                                </div>
+                                <div class="row">
+                                <h3>Color of Border</h3>
+                                <p>#<input name="devloungeWShowBorder" class="input_color" value="<?php echo $devOptions['w_shq_c_b'] ?>"></p>
+                                <small>Enter Color hexcode without #. You can enter 0 for hiding border.</small>
+                                </div>
+                                <div class="row">
+
+                                <h3>Color of Inner Lines</h3>
+                                <p>#<input name="devloungeWInnerBorder" class="input_color" value="<?php echo $devOptions['w_shq_c_i_b'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Font Type</h3>
+                                <p><input name="devloungeWFontType" class="input_font" value="<?php echo $devOptions['w_shq_c_f'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Header Font Color</h3>
+                                <p>#<input name="devloungeWHeaderColor" class="input_color" value="<?php echo $devOptions['w_shq_c_h_c'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Background Color</h3>
+                                <p>#<input name="devloungeWBackgroundColor" class="input_color" value="<?php echo $devOptions['w_shq_c_bg'] ?>"></p>
+                                </div>
+                                <div class="row">
+                                <h3>Font Color</h3>
+                                <p>#<input name="devloungeWFontColor" class="input_color" value="<?php echo $devOptions['w_shq_c_f_c'] ?>"></p>
+                                </div>
+
+                                <div class="row">
+                                <p style="float: left; width:100%;clear: both;margin-top:10px">                                    
+                                    <input type="checkbox" name="devloungeCredits" class="che" value="1" checked="checked"  /> Show Credits
+                                </p>
+                                </div>
+                                <div class="row">
                                 <div class="submit" style="clear: both;">
-                                <input type="submit" name="update_devloungePluginSeriesSettings" value="Update Settings">
+                                <input type="submit" name="update_devloungePluginSeriesSettings" value="Update Settings" class="btn-primary">
+                                </div>
+                                
                                 </div>
 
                                 </form>
-                                <div style="float:left;">Next, <a href="/wp-admin/widgets.php">Choose location of Monitoring Widget</a></div>
+                                <div style="float:left;">Next, <a href="/wp-admin/widgets.php"><strong>Choose location of Monitoring Widget</strong></a></div>
                             </div>
                             <div style="float:left;width:35%">
                                 <p style="font-size: 18px; width: 300px; height: 250px; float: left; margin-top: 20px;">Mockup of Widget<br><br>
-                                <img src="/wp-content/plugins/monitoring/images/mockup.png"></p
+                                <img src="/wp-content/plugins/social-media-monitoring/images/mockup.png"></p
                             </div>
                         </div>
 
@@ -100,13 +273,13 @@ if (class_exists("MonitoringAdmin")) {
 // notice to ping users to come and configure plugin
 function monitoring_admin_notices() {
     echo "<div id='notice' class='updated fade'><p><h2>Social Media Monitoring is not configured yet. Please do it now.</h2>
-          <br/><a href='/wp-admin/admin.php?page=monitoring-menu-configuration'><img src='/wp-content/plugins/monitoring/images/configure.png'></img></a>
+          <br/><a href='/wp-admin/admin.php?page=monitoring-menu-configuration'><img src='/wp-content/plugins/social-media-monitoring/images/configure.png'></img></a>
           </p></div>\n";
 }
 
 // admin panel
 function my_monitoring_menu() {
-    	add_menu_page( 'Configure Social Media Monitoring', 'Monitoring', 'manage_options', 'monitoring-menu-id', 'my_monitoring_options', '/wp-content/plugins/monitoring/images/favicon.png');
+    	add_menu_page( 'Configure Social Media Monitoring', 'Monitoring', 'manage_options', 'monitoring-menu-id', 'my_monitoring_options', '/wp-content/plugins/social-media-monitoring/images/favicon.ico');
 	add_options_page( 'Configure Social Media Monitoring', 'Social Media Monitoring', 'manage_options', 'monitoring-menu-configuration', 'my_monitoring_options' );
 }
 
